e3379ddb9d1adfb0240ff1105a05e18295c925a4
Felix Kirchengast <felix.kirchengast@gmail.com>
2020-01-03 01:46:52 +0100

    Config

---
+++ config.py
@@ -0,0 +1,21 @@
+
+TARGET_REPO = 'ThomasOlip/random-gallery'
+
+# Github only accepts assignees from valid users. We map those users from bitbucket.
+USER_MAPPING = {
+    'martin_gaertner': 'MartinGaertner',
+    'thomas_o': 'ThomasOlip',
+    'fkirc': 'fkirc',
+}
+
+# We map bitbucket's issue "kind" to github "labels".
+KIND_MAPPING = {
+    "task": "enhancement",
+    "proposal": "suggestion",
+}
+
+# The only github statuses are "open" and "closed".
+# Therefore, we map some bitbucket statuses to github "labels".
+STATUS_MAPPING = {
+    "on hold": "suggestion",
+}
